c3ca14e829f43952d4d5cfeddf29d7fd19ce7165
Fernando Ramírez Baquerizo <RBFernando99@gmail.com>
2019-11-25 16:35:38 +0100

    comenzada la implementación de la clase tratamiento

---
+++ Implementacion/codigo/include/tratamiento.cpp
@@ -0,0 +1,7 @@
+#include "tratamiento.hpp"
+
+Tratamiento::Tratamiento(string fechaIncio, string fechaFin, string medicacion) {
+    setFechaInicio(fechaIncio);
+    setFechaFin(fechaFin);
+    setMedicacion(medicacion);
+}
---
+++ Implementacion/codigo/include/tratamiento.hpp
@@ -0,0 +1,15 @@
+#ifndef _TRATAMIENTO_H_
+#define _TRATAMIENTO_H_
+#include <iostream>
+// #include <string>
+
+class Tratamiento {
+private:
+    std::string fechaInicio_;
+    std::string fechaFin_;
+    std::string medicacion;
+public:
+    Tratamiento();
+};
+
+#endif
