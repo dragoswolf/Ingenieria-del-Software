c78945b5d4037cb0387761a0014a43bf88c5d8c6
RBFernando99 <RBFernando00@gmail.com>
2020-01-22 19:54:08 +0100

    pruebas terminadas excepto clase interfaz y test (main a sucio) añadidos

--- Implementacion/codigo/main.cpp
+++ Implementacion/codigo/main.cpp
@@ -1,6 +1,20 @@
 #include "cita.hpp"
+//#include "interfaz.hpp"
+#include "paciente.hpp"
+#include "tratamiento.hpp"
 
-int main(){
-  Cita cita;
-  return 0;
+int main() {
+
+    Paciente nuevoPaciente("dni", "nusha", "nombre", "apellidos", "fechaDeNacimiento", "email", "telefono");
+
+    // Probar con una fecha menor que la de hoy y despues probar con una fecha mayor que la de hoy
+    Cita nuevaCita("dni", "04/12/2020", "motivo");
+    std::cout << nuevaCita.getFecha() << std::endl << std::endl;
+
+    //Podeis probar a hacer perrerias con los tratamientos
+    Tratamiento nuevoTratamiento("medicacion", "12/12/2020", "15/12/2024");
+    std::cout << nuevoTratamiento.getFechaInicio() << std::endl;
+    std::cout << nuevoTratamiento.getFechaFin() << std::endl << std::endl;
+
+    return 0;
 }
